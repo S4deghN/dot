4ff0a68d1e96e94147d0741d85f61040c345a7de
s4 <sadegh.nobakhti@gmail.com>
2022-05-15 11:53:39 +0430

    added install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+ln -svf $PWD/.profile   $HOME/
+ln -svf $PWD/.bashrc    $HOME/
+ln -svf $PWD/.tmux.conf $HOME/
+ln -svf $PWD/nvim       $HOME/.config/
+ln -svf $PWD/alacritty  $HOME/.config/
