253fb794dd05f5186df3ef4bf49832ff2917f27e
s4 <sadegh.nobakhti@gmail.com>
2022-05-19 10:57:55 +0430

    changed the installation method of autostart and applications so that existing autostart and apllications are kept

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/bash
-ln -sfv $PWD/.profile     $HOME/
-ln -sfv $PWD/.bashrc      $HOME/
-ln -sfv $PWD/.tmux.conf   $HOME/
-ln -sfv $PWD/nvim         $HOME/.config/
-ln -sfv $PWD/alacritty    $HOME/.config/
-ln -sfv $PWD/autostart    $HOME/.config/
-ln -sfv $PWD/applications $HOME/.local/share/
+ln -sfv $PWD/.profile       $HOME/
+ln -sfv $PWD/.bashrc        $HOME/
+ln -sfv $PWD/.tmux.conf     $HOME/
+ln -sfv $PWD/nvim           $HOME/.config/
+ln -sfv $PWD/alacritty      $HOME/.config/
+ln -sfv $PWD/autostart/*    $HOME/.config/autostart/
+ln -sfv $PWD/applications/* $HOME/.local/share/applications/
