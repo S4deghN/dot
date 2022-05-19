da25962eff206c686fa5d61a97fc988d4284dec7
s4 <sadegh.nobakhti@gmail.com>
2022-05-19 22:37:01 +0430

    install

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ ln    -sfv $PWD/.bashrc        $HOME/
 ln    -sfv $PWD/.tmux.conf     $HOME/
 ln    -sfv $PWD/nvim           $HOME/.config/
 ln    -sfv $PWD/alacritty      $HOME/.config/
+
+# create directory if it doesn't exist.
+# link every existing file individualy instead of the whole folder so existing files are kept.
 mkdir -p   $HOME/.config/autostart/
 ln    -sfv $PWD/autostart/*    $HOME/.config/autostart/
+
 mkdir -p   $HOME/.local/share/applications/
 ln    -sfv $PWD/applications/* $HOME/.local/share/applications/
