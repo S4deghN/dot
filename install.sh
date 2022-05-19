c2644dc58f29e1808e2a7b07bc77a1d9575dd11a
s4 <sadegh.nobakhti@gmail.com>
2022-05-19 20:34:05 +0430

    install

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/bash
-ln -sfv $PWD/.profile       $HOME/
-ln -sfv $PWD/.bashrc        $HOME/
-ln -sfv $PWD/.tmux.conf     $HOME/
-ln -sfv $PWD/nvim           $HOME/.config/
-ln -sfv $PWD/alacritty      $HOME/.config/
-ln -sfv $PWD/autostart/*    $HOME/.config/autostart/
-ln -sfv $PWD/applications/* $HOME/.local/share/applications/
+ln    -sfv $PWD/.profile       $HOME/
+ln    -sfv $PWD/.bashrc        $HOME/
+ln    -sfv $PWD/.tmux.conf     $HOME/
+ln    -sfv $PWD/nvim           $HOME/.config/
+ln    -sfv $PWD/alacritty      $HOME/.config/
+mkdir -p   $HOME/.config/autostart/
+ln    -sfv $PWD/autostart/*    $HOME/.config/autostart/
+mkdir -p   $HOME/.local/share/applications/
+ln    -sfv $PWD/applications/* $HOME/.local/share/applications/
