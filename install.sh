5b144e5e61557d45ebd9a3eac5315e641dc662b1
s4 <sadegh.nobakhti@gmail.com>
2022-06-23 15:19:44 +0430

    add clnag-format config for global use

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/bash
-ln    -sfv $PWD/.profile       $HOME/
-ln    -sfv $PWD/.bashrc        $HOME/
-ln    -sfv $PWD/.tmux.conf     $HOME/
-ln    -sfv $PWD/nvim           $HOME/.config/
-ln    -sfv $PWD/alacritty      $HOME/.config/
+ln -sfv $PWD/.profile      $HOME/
+ln -sfv $PWD/.bashrc       $HOME/
+ln -sfv $PWD/.tmux.conf    $HOME/
+ln -sfv $PWD/.clnag-format $HOME/
+ln -sfv $PWD/nvim          $HOME/.config/
+ln -sfv $PWD/alacritty     $HOME/.config/
 
 # create directory if it doesn't exist.
 # link every existing file individualy instead of the whole folder so existing files are kept.
