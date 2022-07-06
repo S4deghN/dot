454b87a94cfaa13003a6d710f1c49fbd1970b0d8
S4deghN <sadegh.nobakhti@gmail.com>
2022-07-06 21:24:37 +0430

    add fish

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,32 @@
+function fish_prompt --description 'Write out the prompt'
+    set -l last_status $status
+    set -g fish_prompt_pwd_dir_length 0 # disable shortening
+
+    set_color black
+    echo -n '┌─ '
+    set_color normal
+
+    # PWD
+    # set_color $fish_color_cwd
+    set_color black
+    echo -n (prompt_pwd)
+    set_color normal
+
+    __terlar_git_prompt
+    fish_hg_prompt
+    echo
+
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    end
+
+    set_color black
+    echo -n '└ '
+    set_color normal
+
+    prompt_login
+
+    set_color green
+    echo -n '$ '
+    set_color normal
+end
