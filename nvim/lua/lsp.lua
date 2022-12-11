2e7a0d3d5e63fa193b6bb477cfe19f03b1bd8768
s4 <sadegh.nobakhti@gmail.com>
2022-12-12 00:34:45 +0330

    more default vim like behaviour

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -111,12 +111,12 @@ cmp.setup {
     mapping = cmp.mapping.preset.insert({
         ['<C-n>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                cmp.select_next_item()
             else
                 cmp.complete()
             end
         end),
-        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select });
+        ['<C-p>'] = cmp.mapping.select_prev_item();
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.confirm({ select = true }),
         -- ['<ESC>'] = cmp.mapping.confirm({ select = true }),
