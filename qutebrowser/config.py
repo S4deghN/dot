8d8a9da7496218e9ea7a624dc0abdad4e599c1ef
Zero <sadegh.nobakhti@gmail.com>
2026-06-15 20:20:38 +0330

    dot

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -2243,7 +2243,7 @@ c.url.default_page = 'about:blank'
 ## the search engine name to the search term, e.g. `:open google
 ## qutebrowser`.
 ## Type: Dict
-# c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}'}
+c.url.searchengines = {'DEFAULT': 'https://google.com/?q={}'}
 
 ## Page(s) to open at the start.
 ## Type: List of FuzzyUrl, or FuzzyUrl
